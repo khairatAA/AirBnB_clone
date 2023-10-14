50ef34c598d676a58f441dfa4410b05b609dd3bf
Khairat Adesina <khairatadesina01@gmail.com>
2023-10-15 00:58:42 +0100

    added testcases for place, state, city, amenity and review class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,37 @@
+"""This is the unittest file for place.py file"""
+from datetime import datetime
+from models.place import Place
+import unittest
+
+
+class TestState(unittest.TestCase):
+    """Run tests for the Place class"""
+
+    def setUp(self):
+        """Creates a simple object or instance of Place"""
+        self.my_model = Place()
+
+    def test_types(self):
+        """Test the attribute type"""
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.my_model.city_id, str)
+        self.assertIsInstance(self.my_model.user_id, str)
+        self.assertIsInstance(self.my_model.name, str)
+        self.assertIsInstance(self.my_model.description, str)
+        self.assertIsInstance(self.my_model.number_rooms, int)
+        self.assertIsInstance(self.my_model.number_bathrooms, int)
+        self.assertIsInstance(self.my_model.max_guest, int)
+        self.assertIsInstance(self.my_model.price_by_night, int)
+        self.assertIsInstance(self.my_model.latitude, float)
+        self.assertIsInstance(self.my_model.longitude, float)
+        self.assertIsInstance(self.my_model.amenity_ids, list)
+
+    def test_str_rep(self):
+        """Test the string representation"""
+        output = "[Place] ({}) {}".format(
+            self.my_model.id,
+            self.my_model.__dict__
+        )
+        self.assertEqual(str(self.my_model), output)
