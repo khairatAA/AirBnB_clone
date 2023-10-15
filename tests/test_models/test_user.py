18051b60f7af4481b0cd85f245863c98b309162d
Khairat Adesina <khairatadesina01@gmail.com>
2023-10-15 01:34:42 +0100

    added unittest for User class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,30 @@
+"""This is the unittest file for user.py file"""
+from datetime import datetime
+from models.user import User
+import unittest
+
+
+class TestState(unittest.TestCase):
+    """Run tests for the User class"""
+
+    def setUp(self):
+        """Creates a simple object or instance of User"""
+        self.my_model = User()
+
+    def test_types(self):
+        """Test the attribute type"""
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.my_model.email, str)
+        self.assertIsInstance(self.my_model.password, str)
+        self.assertIsInstance(self.my_model.first_name, str)
+        self.assertIsInstance(self.my_model.last_name, str)
+
+    def test_str_rep(self):
+        """Test the string representation"""
+        output = "[User] ({}) {}".format(
+            self.my_model.id,
+            self.my_model.__dict__
+        )
+        self.assertEqual(str(self.my_model), output)
